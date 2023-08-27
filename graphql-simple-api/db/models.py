e2fe5d95dc506b5fb7e707c495d1d005fba41449
mytysoldier <yoshi008t@gmail.com>
2023-08-27 16:09:24 +0900

    add table

--- graphql-simple-api/db/models.py
+++ graphql-simple-api/db/models.py
@@ -8,3 +8,10 @@ class Hero(SQLModel, table=True):
     name: str
     secret_name: str
     age: Optional[int] = None
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+
+
+class Team(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    headquarters: str
