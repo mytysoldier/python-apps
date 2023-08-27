9f01338a4f13ded86f1e4f915d8c487c02fb610c
mytysoldier <yoshi008t@gmail.com>
2023-08-27 19:08:22 +0900

    update

--- graphql-simple-api/db/db.py
+++ graphql-simple-api/db/db.py
@@ -12,41 +12,65 @@ def initialize_db():
 
 
 def create_heros():
-    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
-    hero_2 = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
-    hero_3 = Hero(name="Rusty-Man", secret_name="Tommy Sharp", age=48)
-
     with Session(engine) as session:
-        session.add(hero_1)
-        session.add(hero_2)
-        session.add(hero_3)
-
+        team_preventers = Team(name="Preventers", headquarters="Sharp Tower")
+        team_z_force = Team(name="Z-Force", headquarters="Sister Margaret’s Bar")
+        session.add(team_preventers)
+        session.add(team_z_force)
         session.commit()
 
-
-def select_heros():
-    with Session(engine) as session:
-        heros = session.exec(
-            select(Hero)
-            .where(col(Hero.age) >= 35)
-            .limit(1)
-            # .where(Hero.secret_name == "Dive Wilson")
-        ).all()
-        print(heros)
+        hero_deadpond = Hero(
+            name="Deadpond", secret_name="Dive Wilson", team_id=team_z_force.id
+        )
+        hero_rusty_man = Hero(
+            name="Rusty-Man",
+            secret_name="Tommy Sharp",
+            age=48,
+            team_id=team_preventers.id,
+        )
+        hero_spider_boy = Hero(name="Spider-Boy", secret_name="Pedro Parqueador")
+        session.add(hero_deadpond)
+        session.add(hero_rusty_man)
+        session.add(hero_spider_boy)
+        session.commit()
 
 
 def update_heros():
     with Session(engine) as session:
         statement = select(Hero).where(Hero.name == "Spider-Boy")
-        results = session.exec(statement)
-        hero = results.one()
-        print("Hero:", hero)
-
-        hero.age = 17
-        session.add(hero)
+        hero_spider_boy = session.exec(statement).one()
+        hero_spider_boy.team_id = 1
+        session.add(hero_spider_boy)
         session.commit()
-        session.refresh(hero)
-        print("Updated hero:", hero)
+        session.refresh(hero_spider_boy)
+        print("Updated hero:", hero_spider_boy)
+
+
+def remove_team_connection():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == "Spider-Boy")
+        hero_spider_boy = session.exec(statement).one()
+        hero_spider_boy.team_id = None
+        session.add(hero_spider_boy)
+        session.commit()
+        session.refresh(hero_spider_boy)
+        print("No longer Preventer:", hero_spider_boy)
+
+
+def select_heros():
+    with Session(engine) as session:
+        statement = select(Hero, Team).where(Hero.team_id == Team.id)
+        results = session.exec(statement)
+        for hero, team in results:
+            print("Hero:", hero, "Team:", team)
+
+
+def select_join_heros():
+    with Session(engine) as session:
+        statement = select(Hero, Team).join(Team, isouter=True)
+        results = session.exec(statement)
+        for hero, team in results:
+            print("Hero:", hero, "Team:", team)
 
 
 def delete_heros():
--- graphql-simple-api/main.py
+++ graphql-simple-api/main.py
@@ -2,7 +2,24 @@ import strawberry
 from fastapi import FastAPI
 from strawberry.asgi import GraphQL
 
-from db.db import create_heros, delete_heros, initialize_db, select_heros, update_heros
+from db.db import (
+    create_heros,
+    delete_heros,
+    initialize_db,
+    remove_team_connection,
+    select_heros,
+    select_join_heros,
+    update_heros,
+)
+
+
+@strawberry.type
+class Hero:
+    id: int
+    name: str
+    secret_name: str
+    age: int
+    team_id: int
 
 
 @strawberry.type
@@ -41,14 +58,26 @@ def heros():
 
 
 @app.get("/select_heros")
-def select_heros():
+def select():
     select_heros()
+    return ""
+
+
+@app.get("/select_join_heros")
+def select_join():
+    select_join_heros()
     return ""
 
 
 @app.get("/update_heros")
 def update():
     update_heros()
+    return ""
+
+
+@app.get("/remove_team_connection")
+def remove_connection():
+    remove_team_connection()
     return ""
 
 
